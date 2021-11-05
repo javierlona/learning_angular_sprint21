d07642014c71ca00d17f6df7ccfbd14e3472b2a2
Javier Lona <javierlona1@gmail.com>
2021-11-05 13:20:24 -0500

    Added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { DetailPageComponent } from './components/detail-page/detail-page.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { MyPageComponent } from './components/my-page/my-page.component';
+import { CreatePageComponent } from './components/create-page/create-page.component';
+import { EditPageComponent } from './components/edit-page/edit-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    DetailPageComponent,
+    ContactPageComponent,
+    MyPageComponent,
+    CreatePageComponent,
+    EditPageComponent
   ],
   imports: [
     BrowserModule,
