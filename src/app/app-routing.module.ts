b727b34bd41dd951adb9642abbd96f4ea8146102
Javier Lona <javierlona1@gmail.com>
2021-11-05 15:30:08 -0500

    Update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { DetailPageComponent } from './components/detail-page/detail-page.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { EditPageComponent } from './components/edit-page/edit-page.component';
+import { MyPageComponent } from './components/my-page/my-page.component';
+import { CreatePageComponent } from './components/create-page/create-page.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomePageComponent, pathMatch: 'full' }
+  { path: '/', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent, pathMatch: 'full' },
+  { path: 'details/:id', component: DetailPageComponent },
+  { path: 'contact/:id', component: ContactPageComponent },
+  { path: 'edit/:id', component: EditPageComponent },
+  { path: 'my-page', component: MyPageComponent },
+  { path: 'create-page', component: CreatePageComponent },
 ];
 
 @NgModule({
