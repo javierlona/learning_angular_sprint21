04e3091da3d684d8715a53fc70f970912138d500
Javier Lona <javierlona1@gmail.com>
2021-11-08 14:09:47 -0600

    Create the getInfo request

--- src/app/services/information.service.ts
+++ src/app/services/information.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InformationService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    ) { }
+
+  getInfo(): Observable<any[]> {
+    console.log("Here in getinfo");
+    console.log(this.http.get<any>('https://jsonplaceholder.typicode.com/todos'));
+    return this.http.get<any>('https://jsonplaceholder.typicode.com/todos');
+  }
 }
